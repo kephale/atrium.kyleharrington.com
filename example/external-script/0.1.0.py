a4d708aa3e8bd1343cf5ad201dd93e5a939019a6
kephale <kephale@users.noreply.github.com>
2024-12-26 14:17:34 +0000

    deploy: da1fcaf6d0e88b652175b2bd162aeed2113037cf

---
+++ example/external-script/0.1.0.py
@@ -0,0 +1,13 @@
+[% if include_remote_script %]
+================
+File: template/example/external-script/0.1.0.py
+================
+# /// script
+# title = "External Script"
+# description = "Script from external repository"
+# external_source = "https://gist.githubusercontent.com/kephale/995a839925299ba1f0304c837066ed2c/raw/a95aef4fc533ce9f002208e9cfac404c634276b7/napari-mandelbrot.py"
+# version = "0.1.0"
+# ///
+
+# This is a metadata-only file pointing to an external script
+[% endif %]
---
+++ mcp_server.py
@@ -0,0 +1,11 @@
+
+from fastmcp import FastMCP
+from typing import Optional, List, Union
+import subprocess
+
+mcp = FastMCP("Demo 🚀")
+
+
+
+if __name__ == "__main__":
+    mcp.run()
